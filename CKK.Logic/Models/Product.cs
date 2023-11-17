b30eb36a8f9550473d957d22f14ccd58e7267285
Salazarco1 <COBY.SALAZAR8160@OMAIL.OTECH.EDU>
2023-11-17 09:38:30 -0700

    11/17/23

---
+++ CKK.Logic/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    public class Product
+    {
+        private int _id;
+        private string _name;
+        private double _price;
+
+        public Product(Product product, string name, double price) 
+        {
+            _id = product._id;
+            _name = name;
+            _price = price;
+        }
+    }
+}
